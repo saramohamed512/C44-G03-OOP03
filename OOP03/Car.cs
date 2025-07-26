8f936522f6d3d7a5dbd817b1f25f360785449fd8
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-26 14:54:36 +0300

    Class

---
+++ OOP03/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP03
+{
+     class Car
+    {
+        //#region Attributes
+        //private int id;
+        //private string model;
+        //private double  speed;
+        //#endregion
+        #region Automatic Properties
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        #endregion
+        #region CLTR
+        public Car(int id, string model , double speed)
+        {   Id = id;
+            Model = model;
+            Speed = speed;
+            Console.WriteLine("1st constructor");
+        }
+        //Constructor overloading
+        public Car(int _id, string _model): this(_id, _model, 0.0)
+        {
+           Console.WriteLine("2nd constructor");
+        }
+        public Car(int _id): this(_id, "Unknown", 0.0)
+        {
+            Console.WriteLine("3rd constructor");
+        }
+
+        #endregion
+        #region methods
+        public override string ToString()
+        {
+                return $"Id: {Id}\nModel: {Model}\nSpeed: {Speed} km/h";
+        }
+        #endregion
+
+    }
+}
--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region UserDefinedType [Class]
+            //Car c1;
+            //c1 = new Car(1, "Toyota", 180.5);
+            //Console.WriteLine(c1);
+            //Car c2 = new Car(2, "Honda");
+            //Console.WriteLine(c2);
+            //Car c3 = new Car(3);
+            //Console.WriteLine(c3);
+            #endregion
         }
     }
 }
