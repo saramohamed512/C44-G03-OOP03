e5c71586cb74741d03026abd8024e83feb7288e9
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-26 15:52:59 +0300

    AccessModifiers

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace OOP03
+﻿using Common;
+namespace OOP03
 {
     internal class Program
     {
@@ -21,13 +22,23 @@
             //Console.WriteLine($"Multiply: {p1.Multiply()}");
             //p1.MyFunc();
 
-            Child child = new Child(10, 20, 30);
-            child.X = 100;
-            child.Y = 200;
-            child.Z = 300;
-            Console.WriteLine(child);
-            Console.WriteLine($"Multiply: {child.Multiply()}");
-            child.MyFunc();
+            //Child child = new Child(10, 20, 30);
+            //child.X = 100;
+            //child.Y = 200;
+            //child.Z = 300;
+            //Console.WriteLine(child);
+            //Console.WriteLine($"Multiply: {child.Multiply()}");
+            //child.MyFunc();
+            #endregion
+            #region AccessModifiers
+            //TypeA typeA = new TypeA();
+            //typeA.A = 10; // Error: A is private in TypeA
+            //typeA.B = 20; // OK: B is internal in TypeA
+            //typeA.C = 30; // OK: C is public in TypeA
+            //typeA.X = 40; // Error: X is private protected in TypeA
+            //typeA.Y = 50; // Error: Y is protected in TypeA
+            //typeA.Z = 60; // Error: Z is protected internal in TypeA
+
             #endregion
         }
     }
