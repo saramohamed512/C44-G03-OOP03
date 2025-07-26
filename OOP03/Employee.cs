74c03b30f2d38f92aac003a93d6716acebe3ef65
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-26 16:16:10 +0300

    problem1

---
+++ OOP03/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP03
+{
+    internal class Employee
+    {
+        #region properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public SecurityLevel SecurityLevel { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime HireDate { get; set; }
+        private char _gender;
+        public char Gender
+        {
+            get { return _gender; }
+            set
+            {
+                if (value == 'M' || value == 'F' || value == 'm' || value == 'f')
+                {
+                    _gender = char.ToUpper(value);
+                }
+                else
+                {
+                   Console.WriteLine("Gender must be either 'M' or 'F'.");
+                }
+            }
+        }
+        #endregion
+        #region constructor
+        public Employee(int id, string name, SecurityLevel securityLevel, decimal salary, DateTime hireDate, char _gender)
+        {
+            Id = id;
+            Name = name;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = _gender;
+        }
+        #endregion
+        #region methods
+        public override string ToString()
+        {
+            return string.Format(
+                "Employee ID: {0}\nName: {1}\nSecurity Level: {2}\nSalary: {3:C}\nHire Date: {4:yyyy-MM-dd}\nGender: {5}",
+                Id, Name, SecurityLevel, Salary, HireDate, Gender);
+        }
+#endregion
+    }
+
+}
--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region problem1:
+            //Employee employee = new Employee(1, "John Doe", SecurityLevel.Developer, 60000.00m, DateTime.Now, 'M');
+            //Console.WriteLine(employee);
+            //employee.Gender = 'X';
+            //Console.WriteLine(employee);
+            #endregion
+
         }
     }
 }
